014f29ab30ce218b239134000709c8735f8783f3
Sagar <sag.sahay@gmail.com>
2015-02-16 21:49:44 +0000

    Commiting ngMadLibs

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+angular.module('madLibsApp',[])
+		.controller('dataController',function($scope){
+			$scope.maleName="";
+			$scope.dirtyTask="";
+			$scope.obnoxiousCelebrity="";
+			$scope.jobTitle="";
+			$scope.celebrity="";
+			$scope.hugeNumber="";
+			$scope.tediousTask="";
+			$scope.uselessSkill="";
+			$scope.adjective="";
+		});
